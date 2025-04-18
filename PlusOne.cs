87e885bb16dde3742630d1b858c2205884bf08dd
ManarShawahni <manarshawahnii@gmail.com>
2025-04-18 23:45:15 +0300

    Update PlusOne.cs

--- PlusOne.cs
+++ PlusOne.cs
@@ -1,15 +1,27 @@
 public class Solution {
     public int[] PlusOne(int[] digits) {
+        bool carry = true;
+
         for (int i = digits.Length - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
+            if (carry) {
                 digits[i]++;
-                return digits;
+                if (digits[i] == 10) {
+                    digits[i] = 0;
+                } else {
+                    carry = false;
+                }
             }
-            digits[i] = 0;
         }
 
-        int[] result = new int[digits.Length + 1];
-        result[0] = 1;
-        return result;
+        if (carry) {
+            int[] result = new int[digits.Length + 1];
+            result[0] = 1;
+            for (int i = 0; i < digits.Length; i++) {
+                result[i + 1] = digits[i];
+            }
+            return result;
+        }
+
+        return digits;
     }
 }
